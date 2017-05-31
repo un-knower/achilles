82402a7f67dea89af0a31c9059f47c815dc85818
zhuzhong <zhuzhong@quancheng-ec.com>
2017-05-31 10:09:41 +0800

    fix bug

--- service/server/src/main/java/com/quancheng/achilles/service/grpc/OdpsSytFlyCheckService.java
+++ service/server/src/main/java/com/quancheng/achilles/service/grpc/OdpsSytFlyCheckService.java
@@ -11,16 +11,16 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundListOperations;
 
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 import com.quancheng.achilles.dao.odps.model.OdpsFlyCheck;
 import com.quancheng.achilles.model.achilles.OdpsSytRequest;
 import com.quancheng.achilles.model.achilles.OdpsSytResponse;
 import com.quancheng.achilles.service.AchillesService;
 import com.quancheng.achilles.service.odps.ODPSQueryService;
 import com.quancheng.achilles.service.redis.RedisComponent;
-import com.quancheng.saluki.boot.SalukiService;
+//import com.quancheng.saluki.boot.SalukiService;
 
-@SalukiService
+//@SalukiService
 public class OdpsSytFlyCheckService implements AchillesService {
     Logger logger = LogManager.getLogger();
     @Resource
@@ -35,27 +35,27 @@ public class OdpsSytFlyCheckService implements AchillesService {
             String redisKey = this.getClass().getName()+":flyCheckStatisticMonth"+":"+odpssytrequest.getClientId()+":"+odpssytrequest.getHappenDate();
             Long size = srt.boundListOps( redisKey).size();
             ArrayList<com.quancheng.achilles.model.achilles.OdpsFlyCheck> resultList =null;
-            Gson parser = new Gson();
-            if(size == null || size == 0 ){
-                List<OdpsFlyCheck> ofcList = odpsService.queryUserCostByClient(odpssytrequest.getClientId(),  odpssytrequest.getHappenDate());
-                BoundListOperations<String, String> redisListOperation = srt.boundListOps( redisKey);
-                if (ofcList != null && !ofcList.isEmpty()) {
-                    resultList = new ArrayList<>( ofcList.size());
-                    com.quancheng.achilles.model.achilles.OdpsFlyCheck ofc = null;
-                    for (OdpsFlyCheck odpsFlyCheck : ofcList) {
-                        ofc = new com.quancheng.achilles.model.achilles.OdpsFlyCheck();
-                        BeanUtils.copyProperties(odpsFlyCheck, ofc);
-                        resultList.add(ofc);
-                        redisListOperation.leftPush(parser.toJson(ofc));
-                    }
-                }
-            }else{
-                List<String> list = srt.boundListOps( redisKey).range(0, size);
-                resultList = new ArrayList<com.quancheng.achilles.model.achilles.OdpsFlyCheck>(size.intValue());
-                for (String json : list) {
-                    resultList.add(parser.fromJson(json, com.quancheng.achilles.model.achilles.OdpsFlyCheck.class));
-                }
-            } 
+//            Gson parser = new Gson();
+//            if(size == null || size == 0 ){
+//                List<OdpsFlyCheck> ofcList = odpsService.queryUserCostByClient(odpssytrequest.getClientId(),  odpssytrequest.getHappenDate());
+//                BoundListOperations<String, String> redisListOperation = srt.boundListOps( redisKey);
+//                if (ofcList != null && !ofcList.isEmpty()) {
+//                    resultList = new ArrayList<>( ofcList.size());
+//                    com.quancheng.achilles.model.achilles.OdpsFlyCheck ofc = null;
+//                    for (OdpsFlyCheck odpsFlyCheck : ofcList) {
+//                        ofc = new com.quancheng.achilles.model.achilles.OdpsFlyCheck();
+//                        BeanUtils.copyProperties(odpsFlyCheck, ofc);
+//                        resultList.add(ofc);
+//                        redisListOperation.leftPush(parser.toJson(ofc));
+//                    }
+//                }
+//            }else{
+//                List<String> list = srt.boundListOps( redisKey).range(0, size);
+//                resultList = new ArrayList<com.quancheng.achilles.model.achilles.OdpsFlyCheck>(size.intValue());
+//                for (String json : list) {
+//                    resultList.add(parser.fromJson(json, com.quancheng.achilles.model.achilles.OdpsFlyCheck.class));
+//                }
+//            } 
             osr.setOdpsFlyCheck(resultList);
             osr.setStatus("success");
         } catch (Exception e) {
