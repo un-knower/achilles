863e4f7b183deb94d72db2c59ac627edc426d7eb
zhuzhong <zhuzhong@quancheng-ec.com>
2017-05-31 10:59:42 +0800

    修改

--- service/server/src/test/achilles/server/TestFieldCopy.java
+++ service/server/src/test/achilles/server/TestFieldCopy.java
@@ -9,11 +9,11 @@ import com.quancheng.achilles.dao.odps.model.OdpsFlyCheck;
 
 public class TestFieldCopy {
     public static void main(String[] args) {
-        long begin =  System.currentTimeMillis();
-        com.quancheng.achilles.model.achilles.OdpsFlyCheck ofc = new com.quancheng.achilles.model.achilles.OdpsFlyCheck();
-        OdpsFlyCheck odpsFlyCheck = new OdpsFlyCheck();
-        odpsFlyCheck.setId(UUID.randomUUID().toString());
-        BeanUtils.copyProperties(odpsFlyCheck, ofc);
-        System.out.println( System.currentTimeMillis() - begin);
+//        long begin =  System.currentTimeMillis();
+//        com.quancheng.achilles.model.achilles.OdpsFlyCheck ofc = new com.quancheng.achilles.model.achilles.OdpsFlyCheck();
+//        OdpsFlyCheck odpsFlyCheck = new OdpsFlyCheck();
+//        odpsFlyCheck.setId(UUID.randomUUID().toString());
+//        BeanUtils.copyProperties(odpsFlyCheck, ofc);
+//        System.out.println( System.currentTimeMillis() - begin);
     }
 }
