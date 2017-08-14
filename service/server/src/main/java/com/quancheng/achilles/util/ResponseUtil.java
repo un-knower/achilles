64e53700990433d243c01d4dec5d2856ffbf5935
zhuzhong <zhuzhong@quancheng-ec.com>
2017-08-14 16:21:19 +0800

    fix bug

--- service/server/src/main/java/com/quancheng/achilles/util/ResponseUtil.java
+++ service/server/src/main/java/com/quancheng/achilles/util/ResponseUtil.java
@@ -1,27 +1,25 @@
 package com.quancheng.achilles.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
-
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.quancheng.achilles.model.basemodel.BaseResponse;
+//import com.quancheng.achilles.model.basemodel.BaseResponse;
 
 public class ResponseUtil {
 
     public static ObjectMapper objectMapper;
-    private static Logger      log = LoggerFactory.getLogger(ResponseUtil.class);
+//    private static Logger      log = LoggerFactory.getLogger(ResponseUtil.class);
 
-    public static BaseResponse setBaseResponse(Boolean success, String message, Integer errorCode) {
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setSuccess(success);
-        if (!StringUtils.isEmpty(message)) {
-            baseResponse.setMessage(message);
-        }
-        if (errorCode != null) {
-            baseResponse.setErrorCode(errorCode);
-        }
-        return baseResponse;
-    }
+//    public static BaseResponse setBaseResponse(Boolean success, String message, Integer errorCode) {
+//        BaseResponse baseResponse = new BaseResponse();
+//        baseResponse.setSuccess(success);
+//        if (!StringUtils.isEmpty(message)) {
+//            baseResponse.setMessage(message);
+//        }
+//        if (errorCode != null) {
+//            baseResponse.setErrorCode(errorCode);
+//        }
+//        return baseResponse;
+//    }
 
 }
