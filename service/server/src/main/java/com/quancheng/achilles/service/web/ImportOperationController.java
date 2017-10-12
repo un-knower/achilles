46a256756b91d8d19e1050dac1ecf9c5e2431a22
zhuzhong <zhuzhong@quancheng-ec.com>
2017-10-12 14:15:31 +0800

    显示组织架构

--- service/server/src/main/java/com/quancheng/achilles/service/web/ImportOperationController.java
+++ service/server/src/main/java/com/quancheng/achilles/service/web/ImportOperationController.java
@@ -2,7 +2,9 @@ package com.quancheng.achilles.service.web;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -21,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.quancheng.achilles.dao.modelwrite.DataItemDetail;
 import com.quancheng.achilles.dao.quancheng_db.model.UcbUser;
 import com.quancheng.achilles.dao.quancheng_db.repository.UcbUserRepository;
 import com.quancheng.achilles.service.constants.InnConstantPage;
@@ -51,11 +55,20 @@ public class ImportOperationController extends ControllerAbstract {
             @ApiParam(value = "每页记录数") @RequestParam(value = "pageSize", required = false, defaultValue = InnConstantPage.PAGE_SIZE_STRING) int pageSize,
             ModelAndView mv) { 
         Page<UcbUser> page = ucbUserRepository.findAll(new PageRequest(pageNum,pageSize,Direction.DESC,"id" ));
+        mv.addObject("sectorList", convert(dataItemServiceImpl.getDataItemDetail("UCB_SECTORS")));
+        mv.addObject("regionList", convert(dataItemServiceImpl.getDataItemDetail("UCB_REGIONS")));
         mv.addObject("page", page);
         mv.setViewName("import/ucb_user_list");
         return mv;
     }
     
+    private Map<Object,String> convert(List<DataItemDetail> items){
+        Map<Object,String> maps=new HashMap<>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            maps.put(items.get(i).getDetailKey(), items.get(i).getDetailText());
+        }
+        return maps;
+    }
     @RequestMapping(value = "/import/ucb/save", method = { RequestMethod.GET, RequestMethod.POST }, produces = {
             MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.TEXT_HTML_VALUE })
     public ModelAndView ucbSave( UcbUser uu,ModelAndView mv) { 
